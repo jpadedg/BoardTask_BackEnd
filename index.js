f2ccf591031d27d888def34eb613265be5370ee1
Aded <joaopedro_aded@hotmail.com>
2023-08-17 19:45:31 -0300

    Tentando concertar erro css deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
-const swaggerOptions =  { customUrl: '/swagger-ui.css' };
+const swaggerOptions =  { customCssUrl: '/swagger-ui.css' };
 const routes = require('./src/routes');
 const authDocProducao = require('./src/middlewares/authDoc');
 const app = express();
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV !== 'test'){
     const swaggerFile = require('./swagger/swagger_output.json');
     app.get('/',  (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc');   })
-    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
 }
 
 
