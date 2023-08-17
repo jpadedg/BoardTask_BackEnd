7fa04e45ee0e0f27afb9af0d9fe52a6592bb9c1f
Aded <joaopedro_aded@hotmail.com>
2023-08-17 19:17:39 -0300

    update index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerUiAssetPath = require('swagger-ui-dist');
+const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
 const swaggerOptions =  { customUrl: '/swagger-ui.css' };
 const routes = require('./src/routes');
 const authDocProducao = require('./src/middlewares/authDoc');
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if(process.env.NODE_ENV !== 'test'){
     const swaggerFile = require('./swagger/swagger_output.json');
     app.get('/',  (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc');   })
-    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions, swaggerUiAssetPath));
+    app.use('/doc',/* authDocProducao, */ swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions, swaggerUiAssetPath));
 }
 
 
