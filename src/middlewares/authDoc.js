85f22238a36164e925dd30220b2559415b911794
Aded <joaopedro_aded@hotmail.com>
2023-08-17 18:31:20 -0300

    Alterações no authDoc.js

--- src/middlewares/authDoc.js
+++ src/middlewares/authDoc.js
@@ -1,33 +1,34 @@
 async function authDocProducao(req, res, next) {
     const { senhaDigitada } = req.body;
 
-    // LOCAL HOST
-    if(req.headers.host.includes("localhost") || req.originalUrl !== "/doc"){
+    if(req.headers.host.includes("localhost") || req.originalUrl !== "/doc/"){
+        // LOCAL HOST
         return next();
     }
 
-    // SENHA CERTA
     if(senhaDigitada === process.env.SWAGGER_SENHA_DOC) {
+        // SENHA CERTA
         return next();
     }
 
-    // SENHA DIGITADA ERRADA
     if(senhaDigitada){
+        // SENHA DIGITADA ERRADA
         res.status(401).set('Content-Type', 'text/html');
         res.send(Buffer.from(`
-            <form method="post>
+            <form method="post">
                 <p style="color: red;">Senha Incorreta!</p>
-                <label for="senhaDigitada">Senha da documentação:"</label>
+                <label for="senhaDigitada">Senha da documentação:</label>
                 <input type="password" name="senhaDigitada" id="senhaDigitada"/>
                 <button type="submit">Entrar</button>
             </form>
-        `))
+        `));
+        
     } else {
         // USUARIO NAO DIGITOU A SENHA E ESTA EM MODO PRODUÇÃO
         res.status(200).set('Content-Type', 'text/html');
         res.send(Buffer.from(`
-            <form method="post>
-                <label for="senhaDigitada">Senha da documentação:"</label>
+            <form method="post">
+                <label for="senhaDigitada">Senha da documentação:</label>
                 <input type="password" name="senhaDigitada" id="senhaDigitada"/>
                 <button type="submit">Entrar</button>
             </form>
