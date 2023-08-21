dc94748c8c79a9945eaea68ba4fdb8680946e82d
Aded <joaopedro_aded@hotmail.com>
2023-08-21 13:49:19 -0300

    Criação do middleware para se conectar ao BD e uma função que retorna erros esperados.

---
+++ src/middlewares/conectarBD.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const tratarErrosEsperados = require('../functions/tratarErrosEsperados.js')
+
+async function conectarBancoDados(req= null, res = null, next= null) {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, { userNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Conectado ao banco de dados!!');
+        try{ next() ; } catch { };
+        return mongoose;
+    } catch (error) {
+        console.log(error);
+        tratarErrosEsperados(res, 'Error: erro ao conectar ao banco de dados')
+        return error;
+    }
+}
+
+module.exports = conectarBancoDados;
